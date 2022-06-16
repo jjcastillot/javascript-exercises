0209e2996b4c350e8305c92465755ebf3dcdc810
jjcastillot <jjcastillot@gmail.com>
2022-06-16 07:57:16 -0500

    Ex5 solved, and commented code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,10 @@
 const sumAll = function(n1,n2) {
+    // Determine if inputs are not numbers or are negative numbers.
     if (typeof(n1)!=='number' || typeof(n2)!=='number' || Math.sign(n1) ===-1 ||
     Math.sign(n2) ===-1) {
         return 'ERROR';
     } else {
+        // Assigns start to the lower number & finish to the higher one
         if ((n2-n1)>=0){
             start=n1;
             finish=n2;
@@ -10,6 +12,7 @@ const sumAll = function(n1,n2) {
             start=n2;
             finish=n1;
         }
+        // Initializes sum and add values from start to finish
         sum =0;
         for (let i=start;i<(finish+1);i++) {
             sum += i;
