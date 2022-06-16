49f7be81c38cca402282e871704f50c16b825d6a
jjcastillot <jjcastillot@gmail.com>
2022-06-16 06:07:20 -0500

    Ex3 and Ex4 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString = '';
+    let len = string.length;
+    for (i=0;i<len;i++){
+        revString += string[len-1-i]; //NEED TO CONCATENATE EACH CHAR
+    }
+    // OPTION 2. Just with built-in functions
+    //return string.split("").reverse().join("");
+    return revString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numArray) {
+    //Uses arguments object to access all the numbers to pop
+    let args = Array.prototype.slice.call(arguments, 1);
+    for (let i=0;i<arguments.length;i++) {
+        for (let j=0;j<numArray.length;j++) {
+            if (numArray[j] === arguments[i]) {
+                numArray.splice(j,1);                  
+            } else {
+                continue;
+            }
+        }
+    }
+    return numArray;
 };
 
 // Do not edit below this line
