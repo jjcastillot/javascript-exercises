6199a486e105e1419f6bf08c3f78f48cd982e72f
jjcastillot <jjcastillot@gmail.com>
2022-06-24 07:00:22 -0500

    Ex8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let suma = 0;
+  let len = numArray.length;
+  for (let i=0;i<len;i++) {
+    suma += numArray[i];
+  }
+  return suma;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  switch (numArray.length) {
+    case 0:
+      return 0;
+      break;
+    case 1:
+      return numArray[0];
+      break;
+    default:
+      mult = 1;
+      for (let i=0;i<numArray.length;i++) {
+        mult *= numArray[i];
+      }
+      return mult;
+      break;
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if ((Math.sign(num)===-1) || (typeof(num)!=='number')) {
+    return 'ERROR';
+  } else if (num===0) {
+    return 1;
+  } else {
+    return num*factorial(num-1);
+  }
 };
 
 // Do not edit below this line
