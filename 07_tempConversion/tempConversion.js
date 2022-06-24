ca972c75707e5bcb061b58c48891036c368d0df0
jjcastillot <jjcastillot@gmail.com>
2022-06-23 22:04:39 -0500

    Ex7 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  if (typeof(temp)!=='number') {
+    return 'ERROR';
+  } else {
+      return Math.round(10*(temp-32)*5/9)/10; // Tricks the round function
+  }    
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  if (typeof(temp)!=='number') {
+    return 'ERROR';
+  } else {
+      return Math.round((temp*9/5+32)*10)/10; // Tricks the round function
+  } 
 };
 
 // Do not edit below this line
