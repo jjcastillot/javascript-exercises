349156d6acf1dd9d185cfe9441cb674bae6f45ed
jjcastillot <jjcastillot@gmail.com>
2022-06-24 07:28:43 -0500

    Ex9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+    // Removes punctuation and spaces from string
+    noPunctString = myString.replace(/[.,\/\ #!$%\^&\*;:{}=\-_`~()]/g,"");
+    fString = noPunctString.toLowerCase();
+    len = fString.length;
+    half_len = Math.floor(fString.length/2);
+    for (let i=0;i<half_len;i++) {
+        if (fString[i]!==fString[len-1-i]) {
+        return false;
+        break;
+        } else {
+            continue;
+        }
+    }
+    return true; 
 };
 
 // Do not edit below this line
