9850836645813a755e6b9dbd356668a48944c58b
jjcastillot <jjcastillot@gmail.com>
2022-08-06 13:19:47 -0500

    Ex10 solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(numS) {
+    let num = parseInt(numS);
+    if ((Math.sign(num)===-1) || (typeof(num)!=='number') || num===0) {
+        return 'OOPS';
+      } else if (num===1 || num===2) {
+        return 1;
+      } else {
+        return fibonacci(num-1) + fibonacci(num-2);
+      }
 };
 
 // Do not edit below this line
