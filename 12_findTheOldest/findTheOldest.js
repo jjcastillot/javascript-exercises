bbc6fab8e2057da22bbd5207810c94ce587c86f9
jjcastillot <jjcastillot@gmail.com>
2022-08-07 14:43:01 -0500

    Ex12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b) {
+        const lastPerson = age(a);
+        const nextPerson = age(b);
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return oldest[0];
 };
+
+function age(obj) {
+    if ('yearOfDeath' in obj) {
+        return obj['yearOfDeath'] - obj['yearOfBirth'];
+    } else {
+        return (new Date()).getFullYear() - obj['yearOfBirth'];
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
